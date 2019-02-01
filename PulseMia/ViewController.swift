c5b66e590bbbd5e13330b6eb47168b528168de28
Gokhan Isiker <gokhanisiker@gmail.com>
2019-02-01 13:50:56 +0000

    Initial commit

---
+++ PulseMia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PulseMia
+//
+//  Created by Gokhan Isiker on 01/02/2019.
+//  Copyright © 2019 Gokhan Isiker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
