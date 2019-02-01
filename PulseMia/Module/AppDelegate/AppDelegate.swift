163c96dbd0db60373238a1463a90bf81f98f99a9
Gokhan Isiker <gokhanisiker@gmail.com>
2019-02-01 14:13:32 +0000

    Clear initial files and directories

---
+++ PulseMia/Module/AppDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PulseMia
+//
+//  Created by Gokhan Isiker on 01/02/2019.
+//  Copyright © 2019 Gokhan Isiker. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
