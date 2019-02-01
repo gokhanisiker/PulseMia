25eb984de34743722ba264a803369bfe22312b88
Gokhan Isiker <gokhanisiker@gmail.com>
2019-02-01 17:17:16 +0000

    Start the app with Contents screen

--- PulseMia/Module/AppDelegate/AppDelegate.swift
+++ PulseMia/Module/AppDelegate/AppDelegate.swift
@@ -15,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        let contents = ["Content1", "Content2", "Content3", "Content4"]
+        let viewModel = ContentViewModel(contents: contents)
+        let viewController = ContentViewController(viewModel: viewModel)
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
     }
 }
